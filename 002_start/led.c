5ef1f6e752bbe586d23cc943fa2a4f20119178bc
tangxinlou <2476790578@qq.com>
2020-08-29 15:40:45 +0800

    传递参数调用c函数点灯

--- 002_start/led.c
+++ 002_start/led.c
@@ -1,13 +1,24 @@
 
 
-
+void delay(volatile int d)
+{
+	while (d--);
+}
 
 int main()
 {
     unsigned int *pGPFCON = (unsigned int *)0x56000050; //无符号int 型指针
 	unsigned int *pGPFDAT = (unsigned int *)0x56000054; 
-
-    *pGPFCON = 0x100;   //gpio 控制器gpf配置寄存器
+    if (which == 4)
+    {
+        /* 配置GPF4为输出引脚 */
+        *pGPFCON = 0x100;
+    }
+    else if (which == 5)
+    {
+        /* 配置GPF5为输出引脚 */
+        *pGPFCON = 0x400;
+    }
 
     *pGPFDAT = 0;       //gpio 控制器gpf 数据寄存器
 
