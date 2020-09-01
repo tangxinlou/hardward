2962e1e1525ca0741f47bf53c74b2fbc66739236
tangxinlou <2476790578@qq.com>
2020-09-01 21:10:09 +0800

    adjust nor flash time of access

--- 003_aal_start/main.c
+++ 003_aal_start/main.c
@@ -1,26 +1,26 @@
 #include "s3c2440_soc.h"
 #include "uart.h"
-
+#include "init.h"
 int main(void)
 {
     unsigned char c;
 
     uart0_init();
-    puts("hello, world!\n\r");
+    puts("Enter the Tacc val:\n\r");
     while(1)
     {
         c = getchar();
-        if(c == '\r')
-        {
-            putchar('\n');
-        }
-
-        if(c == '\n')
-        {
-            putchar('\r');
-        }
-
         putchar(c);
+        if(c >= '0' && c <= '7')
+        {
+            bank0_tacc_set(c - '0');
+            led_test();
+        }
+        else 
+        {
+            puts("Error, val should between 0~7\n\r");
+            puts("Enter the Tacc val: \n\r");
+        }
     }
 
     return 0;
