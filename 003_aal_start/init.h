0e320335a243c7b8d1978f4ffa800c6db66ed205
tangxinlou <2476790578@qq.com>
2020-09-02 22:43:33 +0800

    sdram read and write

--- 003_aal_start/init.h
+++ 003_aal_start/init.h
@@ -2,5 +2,7 @@
 #define _INIT_H
 
 void bank0_tacc_set(int val);
+void sdram_init(void);
+int sdram_test(void);
 
 #endif
--- 003_aal_start/main.c
+++ 003_aal_start/main.c
@@ -6,22 +6,11 @@ int main(void)
     unsigned char c;
 
     uart0_init();
-    puts("Enter the Tacc val:\n\r");
-    while(1)
-    {
-        c = getchar();
-        putchar(c);
-        if(c >= '0' && c <= '7')
-        {
-            bank0_tacc_set(c - '0');
-            led_test();
-        }
-        else 
-        {
-            puts("Error, val should between 0~7\n\r");
-            puts("Enter the Tacc val: \n\r");
-        }
-    }
+    
+    sdram_init();
+
+    if(sdram_test() == 0)
+        led_test();
 
     return 0;
 }
