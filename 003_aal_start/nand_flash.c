baa6368c43ffdbad4a89d5605fcc9edf0bcaf19e
tangxinlou <2476790578@qq.com>
2020-09-20 19:59:47 +0800

    nand flash write erase

--- 003_aal_start/nand_flash.c
+++ 003_aal_start/nand_flash.c
@@ -1,7 +1,7 @@
 #include "s3c240_soc.h"
 #include "my_printf.h"
 
- void nand_init(void)
+void nand_init(void)
 {
 #define  TACLS   0
 #define  TWRPH0  1
@@ -43,13 +43,19 @@ unsigned char nand_data(void)
 	return	NFDATA;
 }
 
+void nand_w_data(unsigned char val)
+{
+	NFDATA = val;
+}
+
+
 void wait_ready(void)
 {
 	while (!(NFSTAT & 1));
 }
 
-
- void nand_chip_id(void)
+ 
+void nand_chip_id(void)
 { 
 	unsigned char buf[5]={0};
 	
@@ -75,15 +81,16 @@ void wait_ready(void)
 	
 }
 
- void nand_read(unsigned int addr, unsigned char *buf, unsigned int len)
+
+void nand_read(unsigned int addr, unsigned char *buf, unsigned int len)
 {
-    int page = addr / 2048;
-    int col = addr & (2048 -1);
-    int i = 0;
+	int i = 0;
+	int page = addr / 2048;
+	int col  = addr & (2048 - 1);
+	
+	nand_select(); 
 
-    nand_select();
-
-       while (i < len)
+	while (i < len)
 	{
 		/* 发出00h命令 */
 		nand_cmd(00);
@@ -115,9 +122,173 @@ void wait_ready(void)
 		col = 0;
 		page++;
 	}
-    nand_deselect();
+	
+	nand_deselect(); 	
 }
- void nand_flash_test(void)
+
+
+int nand_erase(unsigned int addr, unsigned int len)
+{
+	int page = addr / 2048;
+
+	if (addr & (0x1FFFF))
+	{
+		printf("nand_erase err, addr is not block align\n\r");
+		return -1;
+	}
+	
+	if (len & (0x1FFFF))
+	{
+		printf("nand_erase err, len is not block align\n\r");
+		return -1;
+	}
+	
+	nand_select(); 
+
+	while (1)
+	{
+		page = addr / 2048;
+		
+		nand_cmd(0x60);
+		
+		/* row/page addr */
+		nand_addr_byte(page & 0xff);
+		nand_addr_byte((page>>8) & 0xff);
+		nand_addr_byte((page>>16) & 0xff);
+
+		nand_cmd(0xD0);
+
+		wait_ready();
+
+		len -= (128*1024);
+		if (len == 0)
+			break;
+		addr += (128*1024);
+	}
+	
+	nand_deselect(); 	
+	return 0;
+}
+
+void nand_write(unsigned int addr, unsigned char *buf, unsigned int len)
+{
+	int page = addr / 2048;
+	int col  = addr & (2048 - 1);
+	int i = 0;
+
+	nand_select(); 
+
+	while (1)
+	{
+		nand_cmd(0x80);
+
+		/* 发出地址 */
+		/* col addr */
+		nand_addr_byte(col & 0xff);
+		nand_addr_byte((col>>8) & 0xff);
+		
+		/* row/page addr */
+		nand_addr_byte(page & 0xff);
+		nand_addr_byte((page>>8) & 0xff);
+		nand_addr_byte((page>>16) & 0xff);
+
+		/* 发出数据 */
+		for (; (col < 2048) && (i < len); )
+		{
+			nand_w_data(buf[i++]);
+		}
+		nand_cmd(0x10);
+		wait_ready();
+
+		if (i == len)
+			break;
+		else
+		{
+			/* 开始下一个循环page */
+			col = 0;
+			page++;
+		}
+		
+	}
+	
+	nand_deselect(); 	
+}
+
+void do_read_nand_flash(void)
+{
+	unsigned int addr;
+	volatile unsigned char *p;
+	int i, j;
+	unsigned char c;
+	unsigned char str[16];
+	unsigned char buf[64];
+	
+	/* 获得地址 */
+	printf("Enter the address to read: ");
+	addr = get_uint();
+
+	nand_read(addr, buf, 64);
+	p = (volatile unsigned char *)buf;
+
+	printf("Data : \n\r");
+	/* 长度固定为64 */
+	for (i = 0; i < 4; i++)
+	{
+		/* 每行打印16个数据 */
+		for (j = 0; j < 16; j++)
+		{
+			/* 先打印数值 */
+			c = *p++;
+			str[j] = c;
+			printf("%02x ", c);
+		}
+
+		printf("   ; ");
+
+		for (j = 0; j < 16; j++)
+		{
+			/* 后打印字符 */
+			if (str[j] < 0x20 || str[j] > 0x7e)  /* 不可视字符 */
+				putchar('.');
+			else
+				putchar(str[j]);
+		}
+		printf("\n\r");
+	}
+}
+
+void do_erase_nand_flash(void)
+{
+	unsigned int addr;
+	
+	/* 获得地址 */
+	printf("Enter the address of sector to erase: ");
+	addr = get_uint();
+
+	printf("erasing ...\n\r");
+	nand_erase(addr, 128*1024);
+}
+
+
+void do_write_nand_flash(void)
+{
+	unsigned int addr;
+	unsigned char str[100];
+	int i, j;
+	unsigned int val;
+	
+	/* 获得地址 */
+	printf("Enter the address of sector to write: ");
+	addr = get_uint();
+
+	printf("Enter the string to write: ");
+	gets(str);
+
+	printf("writing ...\n\r");
+	nand_write(addr, str, strlen(str)+1);
+
+}
+void nand_flash_test(void)
 {
 	char c;
 
@@ -154,14 +325,17 @@ void wait_ready(void)
 
 			case 'e':
 			case 'E':
+				do_erase_nand_flash();
 				break;
 
 			case 'w':
 			case 'W':
+				do_write_nand_flash();
 				break;
 
 			case 'r':
 			case 'R':
+				do_read_nand_flash();
 				break;
 			default:
 				break;
